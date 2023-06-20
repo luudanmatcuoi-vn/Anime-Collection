4af6241db137026a3d1c33549a59b5d9be42b4b8
Luudan Matcuoi <44934736+luudanmatcuoi-vn@users.noreply.github.com>
2023-06-21 03:26:58 +0700

    new database method

--- mal-gettoken.py
+++ mal-gettoken.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import secrets
+import webbrowser
+from os import path
 
 
 CLIENT_ID = '01a75b8dcdd96c44b126be9fc60c89fc'
@@ -18,7 +20,9 @@ def print_new_authorisation_url(code_challenge: str):
     global CLIENT_ID
 
     url = f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={CLIENT_ID}&code_challenge={code_challenge}'
-    print(f'Authorise your application by clicking here: {url}\n')
+    print(f'Authorise your application by clicking here: {url}')
+    print(f'or we will open it for you\n')
+    webbrowser.open(url)
 
 
 # 3. Once you've authorised your application, you will be redirected to the webpage you've
@@ -57,18 +61,31 @@ def print_user_info(access_token: str):
         'Authorization': f'Bearer {access_token}'
         })
     
-    response.raise_for_status()
+    status = response.raise_for_status()
     user = response.json()
     response.close()
 
     print(f"\n>>> Greetings {user['name']}! <<<")
+    if user['name'] != "":
+        return True
 
 
 if __name__ == '__main__':
+    if path.exists("token.json"):
+        file =open("token.json","r")
+        token = json.load(file)
+        if print_user_info(token['access_token']) == True:
+            exit()
+        else:
+            pass
     code_verifier = code_challenge = get_new_code_verifier()
     print_new_authorisation_url(code_challenge)
 
     authorisation_code = input('Copy-paste the Authorisation Code: ').strip()
+    if "code=" in authorisation_code:
+        index = authorisation_code.index("code=")
+        authorisation_code = authorisation_code[index+5:]
     token = generate_new_token(authorisation_code, code_verifier)
 
     print_user_info(token['access_token'])
+    input()
